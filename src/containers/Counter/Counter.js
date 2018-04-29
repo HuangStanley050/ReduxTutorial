2793a297d9da46c96fedaa4ac1674a18255b368e
Stanley Huang <infamous_godhand@yahoo.com>
2018-04-29 05:23:17 +0000

    added Action Types for the dispatch in a separate file

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 import { connect } from "react-redux";
+import * as actionTypes from "../../store/action.js";
 
 class Counter extends Component {
     /*state = {
@@ -32,8 +33,22 @@ class Counter extends Component {
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this.props.onAdd}  />
-                <CounterControl label="Subtract 5" clicked={this.props.onSubstract}  />
+                <CounterControl label="Add" clicked={this.props.onAdd}  />
+                <CounterControl label="Subtract" clicked={this.props.onSubstract}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => 
+                        //if you want to pass in a parameter in a function and not have it execute it immeidately, you need
+                        //to wrap it in an anonymous function like:
+                        // () => this.props.blah(stuff)
+                        // instead of
+                        //just running this.props.blah(stuff) ----> which will run immediate since it has the () after the function name
+                        <li key={strResult.id} onClick={()=>this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    )}
+                    
+                    
+                </ul>
             </div>
         );
     }
@@ -41,16 +56,19 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
-        onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
-        onAdd: () => dispatch({ type: "ADD" }),
-        onSubstract: () => dispatch({ type: "SUBSTRACT" })
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+        onAdd: () => dispatch({ type: actionTypes.ADD, val: 10 }),
+        onSubstract: () => dispatch({ type: actionTypes.SUBSTRACT, val: 15 }),
+        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, id: id })
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,28 +1,57 @@
+import * as actionTypes from "./action.js";
+
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === "INCREMENT") {
+
+    if (action.type === actionTypes.INCREMENT) {
+        /*
+            const newState = Object.assign({},state);
+            newState.counter = state.counter + 1;
+            return newState;
+        */
         return {
+            ...state,
             counter: state.counter + 1
         };
 
     }
-    if (action.type === "DECREMENT") {
+    if (action.type === actionTypes.DECREMENT) {
         return {
+            ...state,
             counter: state.counter - 1
         };
     }
-    if (action.type === "ADD") {
+    if (action.type === actionTypes.ADD) {
         return {
-            counter: state.counter + 5
+            ...state,
+            counter: state.counter + action.val
         };
     }
-    if (action.type === "SUBSTRACT") {
+    if (action.type === actionTypes.SUBSTRACT) {
         return {
-            counter: state.counter - 5
+            ...state,
+            counter: state.counter - action.val
         };
+    }
+    if (action.type === actionTypes.STORE_RESULT) {
+        //console.log(state.results);
+        return {
+            ...state,
+            results: state.results.concat({ id: new Date(), value: state.counter })
+        }
+    }
+    if (action.type === actionTypes.DELETE_RESULT) {
+        const updatedArray = state.results.filter(result => result.id !== action.id);
+        return {
+            ...state,
+            results: updatedArray
+
+        }
     }
     return state;
 }
